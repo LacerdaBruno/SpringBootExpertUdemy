30015d353c509e5186b2db73cf4b5cdc1ce292ed
BRUNO LACERDA <57118407+LacerdaBruno@users.noreply.github.com>
2022-09-05 21:58:47 -0300

    05/09/2022

--- vendas/src/main/java/org/vendas/service/ClienteService.java
+++ vendas/src/main/java/org/vendas/service/ClienteService.java
@@ -1,5 +1,6 @@
 package org.vendas.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vendas.model.Cliente;
 import org.vendas.repository.ClienteRepository;
@@ -7,11 +8,15 @@ import org.vendas.repository.ClienteRepository;
 @Service
 public class ClienteService {
 
+    private ClienteRepository repository;
+    @Autowired
+    public ClienteService( ClienteRepository repository ){
+        this.repository = repository;
+    }
+
     public void salvarCliente(Cliente cliente){
         validarCliente(cliente);
-        ClienteRepository clienteRepository = new ClienteRepository();
-        clienteRepository.persistir(cliente);
-
+        this.repository.persistir(cliente);
     }
 
     public void validarCliente(Cliente cliente){
